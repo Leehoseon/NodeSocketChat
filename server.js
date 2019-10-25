42191fcafc865d56b683b5dca0062403cef9ffd8
hoseon <a01073591105@github.com>
2019-10-25 16:28:50 +0900

    commit

--- server.js
+++ server.js
@@ -7,24 +7,27 @@ const allSocketEvents = require('./allsocket.js'); // 전체 채팅 추가
 const roomSocketEvents = require('./roomsocket.js'); // 방 채팅 추가
 
 app.use(express.static(path.join(__dirname, 'html'))); //정적 파일들이 있는 위치를 지정 /file.html
-//app.use('/static',express.static(path.join(__dirname, 'html'))); //정적 파일들이 있는 위치를 지정 /static/file.html
 
-app.use(express.static('/socket.io/socket.io.js')); //index.html에서 io.js 파일 사용 가능하도록 설정
+//정적 파일들이 있는 위치를 지정 해당 경로를 http:localhost:8080/static 경로로 사용 가능하도록 설정
+//app.use('/static',express.static(path.join(__dirname, 'html'))); 
+
+//해당 경로를 http:localhost:8080/ 경로로 사용 가능하도록 설정
+app.use(express.static('/socket.io/socket.io.js')); 
 
 //index
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'index.html'));
+  res.sendFile(path.join(__dirname, 'html','chat', 'index.html'));
 });
 
 //all socket
 app.get('/chat', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'all.html'));
+  res.sendFile(path.join(__dirname, 'html','chat', 'all.html'));
 });
 
 
 //room socket
 app.get('/chat', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'room.html'));
+  res.sendFile(path.join(__dirname, 'html','chat', 'room.html'));
 });
 
 /*
